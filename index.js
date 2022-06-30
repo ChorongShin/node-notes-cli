c8e35b466dfd1740d15a14a8dfb443df43e5910f
Chorong Shin <chorongstellashin@gmail.com>
2022-06-30 23:13:40 +0000

    Fisished feature 3

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-// const data = require('./data.json');
+const data = require('./data.json');
 
 const fs = require('fs');
 if(process.argv[2] === 'read'){
+  const updatedData = JSON.stringify(data, null, 2);
   fs.readFile('data.json', 'utf8', (err, data) => {
   if (err){
     console.log(err);
@@ -12,19 +13,32 @@ if(process.argv[2] === 'read'){
   }
 })
 } else if (process.argv[2] === 'create'){
-  const createText = JSON.stringify(process.argv[3])
-
-  fs.writeFile('data.json', createText, 'utf8', (err,data) =>{
-    const object = JSON.parse(data);
-      const objectNotes = object.notes
-  const objectId = object.nextId
-  objectNotes.nextId = createText
-   objectId++
+  const noteId = data.nextId;
+  const newNote = process.argv[3];
+  data.nextId++;
+  data.notes[noteId] = newNote;
+  const updatedData = JSON.stringify(data, null, 2);
+  fs.writeFile('data.json', updatedData, 'utf8', err =>{
+console.log('it/`s working')
     if(err){
       console.log('error:', err)
     }
-    for(const property in object.notes){
-      console.log(`${property}: ${object.notes[property]}`)
+    for(const property in data.notes){
+      console.log(`${property}: ${data.notes[property]}`)
+    }
+})
+} else if (process.argv[2] === 'update') {
+  const noteId = process.argv[3];
+  const updatedNote = process.argv[4];
+  data.notes[noteId] = updatedNote;
+  const updatedData = JSON.stringify(data, null, 2);
+  fs.writeFile('data.json', updatedData, 'utf8', err => {
+    console.log('it/`s working')
+    if (err) {
+      console.log('error:', err)
+    }
+    for (const property in data.notes) {
+      console.log(`${property}: ${data.notes[property]}`)
     }
 })
 }
