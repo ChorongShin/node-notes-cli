22fcc4d8de430ee6111b5885e8bc96341802d8ef
Andre Le <logisticx@yahoo.com>
2022-06-30 22:41:38 +0000

    your turn

--- index.js
+++ index.js
@@ -1,18 +1,30 @@
 // const data = require('./data.json');
 
-
-
 const fs = require('fs');
 if(process.argv[2] === 'read'){
   fs.readFile('data.json', 'utf8', (err, data) => {
   if (err){
     console.log(err);
   }
-  const object = JSON.parse(data);
-
+ const object = JSON.parse(data);
   for (const property in object.notes){
     console.log(`${property}: ${object.notes[property]}`)
   }
+})
+} else if (process.argv[2] === 'create'){
+  const createText = JSON.stringify(process.argv[3])
 
+  fs.writeFile('data.json', createText, 'utf8', (err,data) =>{
+    const object = JSON.parse(data);
+      const objectNotes = object.notes
+  const objectId = object.nextId
+  objectNotes.nextId = createText
+   objectId++
+    if(err){
+      console.log('error:', err)
+    }
+    for(const property in object.notes){
+      console.log(`${property}: ${object.notes[property]}`)
+    }
 })
 }
--- read.js
+++ read.js
@@ -1,9 +1,5 @@
 const fs = require('fs');
-for(let i=0;i<data.json.notes.length;i++){
- fs.readFile(data.json.notes[i], 'utf8', (err, data) => {
+ fs.readFile('data.json', 'utf8', (err, data) => {
   if (err) throw err;
   console.log(data);
-
-
 })
-}
